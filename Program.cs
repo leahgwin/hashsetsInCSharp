59e96f302d13ae599bc604cf0aeb023422d0797f
leahgwin <leahgwin@gmail.com>
2018-08-29 10:16:00 -0500

    made the first hashset and have it printing the showroom cars and count

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use the UnionWith() method on Showroom to add in the two models you added to UsedLot.
+            // You've sold one of your cars. Remove it from the set with the Remove() method.
+
+
+            // Create an empty HashSet named Showroom that will contain strings.
+
+            HashSet<string> Showroom = new HashSet<string> { };
+            // Add four of your favorite car model names to the set.
+            Showroom.Add("Element");
+            Showroom.Add("CRV");
+            Showroom.Add("Pilot");
+            Showroom.Add("Prius");
+
+            // Print to the console how many cars are in your show room.
+            Console.WriteLine(Showroom.Count);
+
+            // Pick one of the items in your show room and add it to the set again.
+            Showroom.Add("Element");
+
+            // Print your showroom again, and notice how there's still only one representation of that model in there.
+            foreach (String model in Showroom)
+            {
+                Console.WriteLine(model);
+            }
+
+            // Create another set named UsedLot and add two more car model strings to it.
+            HashSet<string> UsedLot = new HashSet<string> { };
+            UsedLot.Add("Model X");
+            UsedLot.Add("Model S");
+
+
+
+        }
+    }
+}
